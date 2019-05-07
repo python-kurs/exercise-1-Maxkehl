a8caf0f62c286a705b90a3ddc78f836e8af030aa
Max Kaminkehrer <max.kehl@posteo.de>
2019-05-07 10:29:49 +0200

    ex1_done

--- first_steps.py
+++ first_steps.py
@@ -2,20 +2,31 @@
 # Answers to questions that ask for non-code answers can simply be added as comments.
 
 # 1) The following line causes a SyntaxError. Please correct the line so that it's output is 'Hallo Welt!' [1P]
-print(Hallo Welt!)
+print('Hallo Welt!')
+
 
 # 2) Calculate the difference between a and b and assign the result to a variable called 'v_1'. What are the datatypes of a, b and 'v_1'? [2P]
 a = 976.543
 b = 345
+v_1 = a - b
+# a = floar
+# b = integer
+# v_1 = float
 
 # 3) Calculate the remainder of the division 100/17 by only using one operator and save the result in v_2 (look into the Python docs for help) [1P]
+v_2 = 100 % 7
 
 # 4) The speed of light is about 300'000 km/s. Calculate the wavelength (in nanometers) of a light wave with a frequency of 5.0E+14 per second. Can we see this light? [4P]
 #    Save the result in v_3.
+v = 3.0E+8 # in m/s
+f = 5.0E+14 
+v_3 = (v / f)* 1.0E+9 # Umerechnung in Nanometer
+# V_3 = 600.0nm ;sichbares Lich: 380 - 780 nm -> Ja
 
 # 5) Print the following string to the console using the format-function and the variables 'n' and 'pi': "Pi rounded to the first 10 decimals is: 3.1415926536" [2P]
 n  = 10
 pi = 3.14159265358979323846264338
+print('Pi rounded to the first {:0d} decimals is {:012.10f}'.format(n,pi))
 
 # ------------------------------------
 # - Don't change stuff below this line
